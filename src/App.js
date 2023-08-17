16253e6e4fb0b9fbbc50259fb926c3de2097bff4
Paulo-vca <paulovca26@gmail.com>
2023-08-17 02:30:56 -0400

    cont

--- src/App.js
+++ src/App.js
@@ -1,56 +1,63 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable no-useless-constructor */
-import "./App.css";
-import { Component } from "react";
+import './App.css'
+import { render } from '@testing-library/react'
+import { Component } from 'react'
 
 class App extends Component {
-  state = {
-    posts: [],
-  };
+    state = {
+        posts: []
+    }
 
-  // fetch aqui
-  componentDidMount() {
-    
-    setTimeout(() => {
-      this.setState({
-        counter: 0,
-        posts: [
-          {
-            id: 1,
-            title: "Título 1",
-            body: "Corpo 1",
-          },
-          {
-            id: 2,
-            title: "Título 2",
-            body: "Corpo 2",
-          },
-          {
-            id: 3,
-            title: "Título 3",
-            body: "Corpo 3",
-          },
-        ],
-      });
-    }, 2000)
+    componentDidMount() {
+        this.loadPosts()
+    }
 
-    
-  }
+    loadPosts = async () => {
+        const postsResponse = fetch(
+            'https://jsonplaceholder.typicode.com/posts'
+        )
 
-  render() {
-    const { posts, counter } = this.state;
+        const photosResponse = fetch(
+            'https://jsonplaceholder.typicode.com/photos'
+        )
 
-    return (
-      <div className="App">
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <h3>{post.body}</h3>
-          </div>
-        ))}
-      </div>
-    );
-  }
+        const [posts, photos] = await Promise.all([
+            postsResponse,
+            photosResponse
+        ])
+
+        const postsJson = await posts.json()
+        const photosJson = await photos.json()
+
+        // Array para armazenar os posts e fotos
+        const photosAndPosts = postsJson.map((post, index) => {
+            return {
+                ...post,
+                cover: photosJson[index].url
+            }
+        })
+
+        this.setState({ posts: photosAndPosts })
+    }
+
+    render() {
+        const { posts } = this.state
+
+        return (
+            <section className="container">
+                <div className="posts">
+                    {posts.map(post => (
+                        <div key={post.id} className="post">
+                            <img src={post.cover} alt="" />
+                            <div className="text">
+                                <h1>{post.title}</h1>
+                                <h3>{post.body}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        )
+    }
 }
 
-export default App;
+export default App
